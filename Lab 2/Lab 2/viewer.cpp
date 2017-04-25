ffec835c965fd349cc786fb49f6edc090d6930f4
Rhys Agombar <Rhys.Agombar@uoit.net>
2017-04-25 16:01:21 -0400

    Trimmed Excess Files
    Only Lab 2 folder is required. Zip and Report kept for record purposes.

--- Lab 2/Lab 2/viewer.cpp
+++ Lab 2/Lab 2/viewer.cpp
@@ -373,10 +373,10 @@ void init() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibuffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, i_size, inds, GL_STATIC_DRAW);
 
+	glUseProgram(program);
 	vPosition = glGetAttribLocation(program, "vPosition");
 	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(vPosition);
-
 	vNormal = glGetAttribLocation(program, "vNormal");
 	glVertexAttribPointer(vNormal, 3, GL_FLOAT, GL_FALSE, 0, (void*)v_size);
 	glEnableVertexAttribArray(vNormal);
